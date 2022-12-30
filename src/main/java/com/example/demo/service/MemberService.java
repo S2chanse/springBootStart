f06f6010dd4cf7850f7f3216ccf2e6a250bf6278
S2chanse <seop7379@daum.net>
2022-12-30 17:28:04 +0900

    make service class

---
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -0,0 +1,40 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.Member;
+import com.example.demo.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    public final MemoryMemberRepository mmr = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join (Member member){
+        validateDubplicate(member);//중복체크
+        mmr.save(member);
+        return member.getId();
+    }
+
+    private void validateDubplicate(Member member) {
+        mmr.findByName(member.getName())
+                .ifPresent(m->{//존재여부 확인
+                    throw  new IllegalStateException("이미 존재하는 유저입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     * */
+    public List<Member> findMembers(Member member){
+        return mmr.findAll();
+    }
+    /**
+     * 회원 조회
+     */
+    public Optional<Member> findMember(String name){
+        return mmr.findByName(name);
+    }
+}
